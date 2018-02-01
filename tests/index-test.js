7bb72012b6a4b04c84dd8f9b0a2c302de84b9013
Peter <peter@praekelt.com>
2018-02-01 16:57:00 +0100

    Wrote some tests for the url parser

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,9 +1,82 @@
-import expect from 'expect'
+import expect from 'expect';
 
-import message from 'src/index'
+import { convertRESTRequestToHTTP } from 'src/index';
 
-describe('Module template', () => {
-  it('displays a welcome message', () => {
-    expect(message).toContain('Welcome to aor-drf-client')
-  })
-})
+let apiUrl = 'http://test.com/api/v1';
+
+describe('Pagination', () => {
+    it('param encodes pagination for 2 items on page 2', () => {
+        var x = 1;
+        let outputUrl = convertRESTRequestToHTTP({
+            apiUrl,
+            type: 'GET_LIST',
+            resource: 'users',
+            params: {
+                pagination: {
+                    page: 2,
+                    perPage: 2
+                }
+            }
+        });
+        expect(outputUrl).toEqual({
+            options: {},
+            url: `${apiUrl}/users/?limit=2&offset=4`
+        });
+    });
+});
+describe('Sorting', () => {
+    it('param encodes ascending ordering by name', () => {
+        var x = 1;
+        let outputUrl = convertRESTRequestToHTTP({
+            apiUrl,
+            type: 'GET_LIST',
+            resource: 'users',
+            params: {
+                sort: {
+                    field: 'name',
+                    order: 'ASC'
+                }
+            }
+        });
+        expect(outputUrl).toEqual({
+            options: {},
+            url: `${apiUrl}/users/?ordering=name`
+        });
+    });
+
+    it('param encodes descending ordering by name', () => {
+        var x = 1;
+        let outputUrl = convertRESTRequestToHTTP({
+            apiUrl,
+            type: 'GET_LIST',
+            resource: 'users',
+            params: {
+                sort: {
+                    field: 'name',
+                    order: 'DESC'
+                }
+            }
+        });
+        expect(outputUrl).toEqual({
+            options: {},
+            url: `${apiUrl}/users/?ordering=-name`
+        });
+    });
+});
+describe('Search', () => {
+    it('param encodes search query for "test"', () => {
+        var x = 1;
+        let outputUrl = convertRESTRequestToHTTP({
+            apiUrl,
+            type: 'GET_LIST',
+            resource: 'users',
+            params: {
+                filter: 'test'
+            }
+        });
+        expect(outputUrl).toEqual({
+            options: {},
+            url: `${apiUrl}/users/?search=test`
+        });
+    });
+});
